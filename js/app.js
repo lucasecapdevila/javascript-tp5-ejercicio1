72e61244618a0b6afe19ee4be5787cddcbb21db7
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-19 18:56:02 -0300

    se terminan las funcionalidades solicitadas

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
-const generarNumeroAleatorio = () =>{
-  let numAleatorio = Math.floor(Math.random() * 10) + 1
-  console.log(numAleatorio);
+const numAleatorio = Math.floor(Math.random() * 10) + 1
+const btnEmpezar = document.querySelector('.btn-danger')
+
+const empezarJuego = () =>{
+  const p = document.createElement('p')
+  p.classList.add('lead', 'my-2')
+  p.innerText = `El juego empezó ! Debe adivinar el número aleatorio que se generó entre el 1 y el 10`
+  btnEmpezar.insertAdjacentElement("afterend", p)
 }
 
-const nuevoContenido = () =>{
-  console.log('nuevoContenido');
+const coincidenNumeros = () =>{
+  let numUsuario = document.getElementById('numUsuario')
+  if(numAleatorio > numUsuario.value){
+    alert('El número aleatorio es mayor al que usted ingresó');
+  } else if(numAleatorio < numUsuario.value){
+    alert('El número aleatorio es menor al que usted ingresó');
+  } else{
+    alert('Acertaste el número secreto !');
+  }
 }
 
-const btnEmpezarJuego = document.getElementsByClassName('btn-danger')
+const numUsuario = document.getElementById('btn-enviar')
+numUsuario.addEventListener('click', coincidenNumeros)
